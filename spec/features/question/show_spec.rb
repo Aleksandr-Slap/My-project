4b929e99200dd03642810200746350ad3f29ed96
KALi <aleksandr-slap@rambler.ru>
2022-06-30 12:55:36 +0300

    created feature test 'The user can view the list of questions'

---
+++ spec/features/question/show_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+feature 'User can view list of questions', %q{
+  To solve their problem, the user wants to 
+  see a list of questions
+} do
+
+  given!(:questions) {create_list(:question, 3)}
+
+  scenario 'User sees list of questions' do 
+    visit questions_path
+
+    questions.each { |q| expect(page).to have_content q.body }
+  end  
+end  
