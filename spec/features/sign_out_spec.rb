90de673e00017b5cc47eb2b40144728b36472540
KALi <aleksandr-slap@rambler.ru>
2022-07-06 21:27:48 +0300

    created feature test 'User can sign out'

---
+++ spec/features/sign_out_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+feature 'User can sign out', %q{
+  I want to exit the app to change 
+  my account or create a new one
+} do
+
+  given(:user) { create(:user) }  
+
+  scenario 'User sign out' do
+    sign_in(user)
+    click_on 'Exit'
+    expect(page).to have_content 'Signed out successfully.'
+  end  
+end  
