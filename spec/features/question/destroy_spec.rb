e08789c36db9a004e3f714f1618f643605dfa733
KALi <aleksandr-slap@rambler.ru>
2022-07-09 15:43:05 +0300

    refactoring featue test 'User can delete his question'

--- spec/features/question/destroy_spec.rb
+++ spec/features/question/destroy_spec.rb
@@ -5,20 +5,16 @@ feature 'User can delete his question', %q{
   question, he can delete it
 } do
 
+  given(:user) { create(:user) }
+  let!(:question) {create(:question, user: user)}
+
   scenario 'User deletes his question' do
-    user = create(:user)
     sign_in(user)
 
-    click_on 'Ask question'
+    visit question_path(question)
 
-    fill_in 'Title', with: 'Test question'
-    fill_in 'Body', with: 'text text text'
-    click_on 'Ask'
+    save_and_open_page
 
-    expect(page).to have_content 'Your question successfuly created.'
-    expect(page).to have_content 'Test question'
-    expect(page).to have_content 'text text text'
-    
     click_on 'Delete'
     expect(page).to have_content 'Question successfully deleted.'
     expect(page).to have_content 'All questions'
